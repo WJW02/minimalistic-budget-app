03c9bb8c28eef399c29a116523b146dd3fa53dec
WJW02 <wng.mattia@gmail.com>
2023-08-20 08:44:18 +0200

    Define BudgetItem class

--- src/model/BudgetItem.java
+++ src/model/BudgetItem.java
@@ -1,5 +1,31 @@
 package model;
-
+import java.util.Date;
 public class BudgetItem {
+    private Date date;
+    private String description;
+    private int amount;
 
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
 }
