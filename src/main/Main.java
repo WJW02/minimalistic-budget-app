33a6c16e6ce08783feef3186a7275fa9f3bfbd28
WJW02 <wng.mattia@gmail.com>
2023-09-13 11:42:37 +0200

    Put the Main class in the package main

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,20 @@
+package main;
+
 import model.Model;
 import view.View;
 import controller.Controller;
+
 /**
- * Documentation
+ * Main class
+ *
+ * @author WJW02
  */
 public class Main {
+    /**
+     * Initializes the Model-View-Controller environment
+     *
+     * @param args The command line arguments
+     */
     public static void main(String[] args) {
         Model model = new Model();
         View view = new View(model.getTableModel());
