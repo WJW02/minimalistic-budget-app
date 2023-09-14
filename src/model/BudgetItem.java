e74303ff46476f3bc4fe27ca229f11d9d696c684
WJW02 <wng.mattia@gmail.com>
2023-09-14 10:29:52 +0200

    Add javadoc comments to the source code of the model package

--- src/model/BudgetItem.java
+++ src/model/BudgetItem.java
@@ -2,11 +2,34 @@ package model;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
+/**
+ * The basic unit of a {@link Model}.
+ * <p>
+ * It represents a row in a {@link Model#getTableModel()}.
+ *
+ * @author WJW02
+ */
 public class BudgetItem {
+    /**
+     * The date of the item.
+     */
     private LocalDate date;
+    /**
+     * The description of the item.
+     */
     private String description;
+    /**
+     * The amount of the item.
+     */
     private BigDecimal amount;
 
+    /**
+     * Creates a BudgetItem.
+     *
+     * @param date the date of the item
+     * @param description the description of the item
+     * @param amount the amount of the item
+     */
     public BudgetItem(LocalDate date, String description, BigDecimal amount) {
         this.date = date;
         this.description = description;
--- src/model/Model.java
+++ src/model/Model.java
@@ -2,12 +2,27 @@ package model;
 
 import javax.swing.table.DefaultTableModel;
 
+/**
+ * The class providing the model of the application.
+ * <p>
+ * The model uses a DefaultTableModel over a TableModel because it has built-in
+ * support for dynamic data manipulation and basic sorting/filtering.
+ *
+ * @author WJW02
+ */
 public class Model {
-    // Using DefaultTableModel over TableModel because it has built-in
-    // support for dynamic data manipulation and basic sorting/filtering
+    /**
+     * The actual underlying model used to keep the data.
+     */
     private DefaultTableModel tableModel;
+    /**
+     * The list of names of the columns of {@link #tableModel}.
+     */
     private String[] columnNames = {"Date", "Description", "Amount"};
 
+    /**
+     * Initializes {@link #tableModel} with no rows and not editable cells.
+     */
     public Model() {
         tableModel = new DefaultTableModel(columnNames, 0) {
             @Override
@@ -22,10 +37,22 @@ public class Model {
         return tableModel;
     }
 
+    /**
+     * Adds a new row to {@link #tableModel}.
+     *
+     * @param item the item added to tableModel
+     */
     public void addRow(BudgetItem item) {
         tableModel.addRow(new Object[]{item.getDate(), item.getDescription(), item.getAmount()});
     }
 
+    /**
+     * Compares this model with another model.
+     *
+     * @param model the model it's compared to
+     * @return <code>true</code> - if their {@link #tableModel} contain the same values in the same order<br>
+     * <code>false</code> - otherwise
+     */
     public boolean equals(Model model) {
         DefaultTableModel tableModel1 = this.getTableModel();
         DefaultTableModel tableModel2 = model.getTableModel();
